beec31f04d8cc6482e125b0f58cf28244724a10d
adelorenzo-portainer <81579885+adelorenzo-portainer@users.noreply.github.com>
2023-05-16 20:40:59 -0600

    Create portainer_hb.sh

---
+++ portainer_hb.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+
+clear
+
+while true
+do
+portainer_up=`curl --silent --insecure -X GET https://192.168.10.171:30779/api/endpoints | jq -r '.details'`
+if [ "$portainer_up" = "Unauthorized" ]; then
+        echo -ne '⚡ Portainer is up\r'
+else
+        break
+fi
+sleep 5
+done
+
+kubectl apply -n portainer -f https://downloads.portainer.io/ee2-18/portainer.yaml
+echo
+echo 'Deploying Portainer server'
+echo
+
+while true
+do
+portainer_running=`kubectl get po -n portainer | tail -1 | awk '{print $3}'`
+if [ "$portainer_running" != "Running" ]; then
+        echo -ne ' ⚡ Portainer is Not Running yet\r'
+else
+        break
+fi
+sleep 1
+done
+
+sleep 5
+
+echo
+echo 'Restoring Portainer backup'
+
+curl -X POST \
+--insecure \
+--header 'Content-Type: application/json' \
+--url 'https://192.168.10.176:30779/api/backup/s3/restore' \
+--data '{"accessKeyID": "portainer", "bucketName": "kubernetes1",  "filename": "portainer-backup_2023-05-17_00-55-00.tar.gz",  "password": "",  "region": "us-east-1",  "s3CompatibleHost": "http://192.168.10.1:9002",  "secretAccessKey": "HmLzJ3evHclFoG4w"}'
+
+echo
+echo 'Portainer restored'
